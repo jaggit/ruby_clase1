c2d7bf7a630ad9559f3dd0cef3b00ee96c48a7a3
mppfiles <mppfiles@gmail.com>
2012-10-19 20:26:50 -0300

    primer comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'cbbd7ea2f350d10c11ec32a8f1a243f3dc2ad3c3f5158ce1da37bf6d9d8890ff76430026ae6dcb4bda5ab6cd0c2b331de7c0ff8d2bd8a1efd5fa1b97b27abb35'
